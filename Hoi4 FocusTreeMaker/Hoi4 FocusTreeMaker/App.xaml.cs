38ecd4e9d894d4dad7d73d0131067ec101080986
DSDHX <56496118+DSDHX@users.noreply.github.com>
2025-04-07 20:04:56 -0700

    初始项目创建

---
+++ Hoi4 FocusTreeMaker/Hoi4 FocusTreeMaker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Hoi4_FocusTreeMaker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
