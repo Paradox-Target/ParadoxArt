9675c90e7501e011bd18fdc589bf7afe1c7f3f4e
DSDHX <56496118+DSDHX@users.noreply.github.com>
2025-06-09 11:49:31 -0700

    初始设置

--- Hoi4BlueprintEditor/Views/MainWindow.xaml.cs
+++ Hoi4BlueprintEditor/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hoi4BlueprintEditor
+namespace Hoi4BlueprintEditor.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
