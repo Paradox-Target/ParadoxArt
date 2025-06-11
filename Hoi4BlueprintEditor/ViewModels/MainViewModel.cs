ca8f1c46dfbe733b7cfc73455cb6a95dd1221b2a
textGamex <textGamex@outlook.com>
2025-06-12 01:36:15 +0800

    refactor: 使用RelayCommand替换手动构造

--- Hoi4BlueprintEditor/ViewModels/MainViewModel.cs
+++ Hoi4BlueprintEditor/ViewModels/MainViewModel.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Globalization;
 
 namespace Hoi4BlueprintEditor.ViewModels
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
-        public ICommand SetLanguageToEnglishCommand { get; }
-        public ICommand SetLanguageToChineseCommand { get; }
-
-        public MainViewModel()
-        {
-            SetLanguageToEnglishCommand = new RelayCommand(SetLanguageToEnglish);
-            SetLanguageToChineseCommand = new RelayCommand(SetLanguageToChinese);
-        }
-
         private void SetLanguage(string cultureCode)
         {
             var newCulture = new CultureInfo(cultureCode);
@@ -30,7 +21,10 @@ namespace Hoi4BlueprintEditor.ViewModels
             CultureInfo.CurrentCulture = newCulture;
         }
 
+        [RelayCommand]
         private void SetLanguageToEnglish() => SetLanguage("en-US");
+
+        [RelayCommand]
         private void SetLanguageToChinese() => SetLanguage("zh-CN");
     }
 }
