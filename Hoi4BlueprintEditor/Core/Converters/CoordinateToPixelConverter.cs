2de7d517e1ccacfff9f7f326733a1cdd956bc5dd
textGamex <textGamex@outlook.com>
2025-06-16 17:26:34 +0800

    fix: change NotImplementedException to NotSupportedException in ConvertBack method

--- Hoi4BlueprintEditor/Core/Converters/CoordinateToPixelConverter.cs
+++ Hoi4BlueprintEditor/Core/Converters/CoordinateToPixelConverter.cs
@@ -20,6 +20,6 @@ public sealed class CoordinateToPixelConverter : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
