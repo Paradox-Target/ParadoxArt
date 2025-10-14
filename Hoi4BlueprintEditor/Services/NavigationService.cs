62395edd04392374a291c2302c9092468c47041f
textGamex <textGamex@outlook.com>
2025-10-14 10:23:41 +0800

    feat: 添加导航服务 & 重构MainWindow

---
+++ Hoi4BlueprintEditor/Services/NavigationService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Hoi4BlueprintEditor.Services;
+
+[RegisterSingleton<NavigationService>]
+public sealed class NavigationService
+{
+    public object? CurrentView
+    {
+        get => _currentView;
+        private set
+        {
+            _currentView = value;
+            ViewChanged?.Invoke();
+        }
+    }
+
+    public event Action? ViewChanged;
+    private object? _currentView;
+    public void NavigateTo<TView>()
+    {
+        NavigateTo(typeof(TView));
+    }
+
+    private void NavigateTo(Type type)
+    {
+        CurrentView = App.Current.Services.GetRequiredService(type);
+    }
+}
---
+++ Hoi4BlueprintEditor/ViewModels/MainControlViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Hoi4BlueprintEditor.Messages;
+using Hoi4BlueprintEditor.Services;
+using Microsoft.Win32;
+
+namespace Hoi4BlueprintEditor.ViewModels;
+
+[RegisterSingleton<MainControlViewModel>]
+public sealed partial class MainControlViewModel : ObservableObject 
+{
+    private readonly SettingsService _settingsService;
+    private readonly AppLocalizationService _appLocalizationService;
+
+    public MainControlViewModel(SettingsService settingsService, AppLocalizationService appLocalizationService)
+    {
+        _settingsService = settingsService;
+        _appLocalizationService = appLocalizationService;
+    }
+
+    private void SetLanguage(string cultureCode)
+    {
+        var targetCulture = new CultureInfo(cultureCode);
+
+        string message = _appLocalizationService.GetString("LanguageRestartMessage", targetCulture);
+        string title = _appLocalizationService.GetString("RestartRequiredTitle", targetCulture);
+
+        _settingsService.Language = cultureCode;
+        _settingsService.SaveSettings();
+
+        MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    [RelayCommand]
+    private void SetLanguageToEnglish() => SetLanguage("en-US");
+
+    [RelayCommand]
+    private void SetLanguageToChinese() => SetLanguage("zh-CN");
+
+    [RelayCommand]
+    private void OpenFocusFile()
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "Focus Files (*.txt)|*.txt|All Files (*.*)|*.*",
+            Title = "选择国策树文件",
+            Multiselect = false,
+        };
+
+        if (openFileDialog.ShowDialog() != true)
+        {
+            return;
+        }
+
+        string filePath = openFileDialog.FileName;
+        WeakReferenceMessenger.Default.Send(new OpenFileMessage(filePath));
+    }
+
+    [RelayCommand]
+    private void SaveFocusFile()
+    {
+        WeakReferenceMessenger.Default.Send(new SaveFocusTreeMessage(""));
+    }
+}
