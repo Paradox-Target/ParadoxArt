02718cd2577120da17360973e4791d9178f98178
textGamex <textGamex@outlook.com>
2025-11-10 16:56:57 +0800

    feat: 添加 JetBrains.Annotations 包并标记通知服务方法为需要本地化

--- Hoi4BlueprintEditor/Services/NotificationService.cs
+++ Hoi4BlueprintEditor/Services/NotificationService.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿using JetBrains.Annotations;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Hoi4BlueprintEditor.Services;
 
 [RegisterSingleton<NotificationService>]
 public sealed class NotificationService : IDisposable
 {
+    [LocalizationRequired]
     public void Show(string message)
     {
         new ToastContentBuilder().AddText(message).Show();
