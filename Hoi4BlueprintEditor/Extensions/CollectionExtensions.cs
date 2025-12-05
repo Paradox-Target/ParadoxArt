3b7c569ea5f10221df348c70bf0f992aa584b0c3
textGamex <textGamex@outlook.com>
2025-12-05 21:52:38 +0800

    perf: 移动无任何连接线国策时不重新渲染连接线

---
+++ Hoi4BlueprintEditor/Extensions/CollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Hoi4BlueprintEditor.Extensions;
+
+public static class CollectionExtensions
+{
+    extension<T>(IReadOnlyCollection<T> collection)
+    {
+        public bool IsEmpty => collection.Count == 0;
+        public bool IsNotEmpty => collection.Count != 0;
+    }
+}
