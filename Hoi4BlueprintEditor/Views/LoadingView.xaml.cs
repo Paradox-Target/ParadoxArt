2d28285361d0b6443f1c4c8f9153f2047a798b65
textGamex <textGamex@outlook.com>
2025-12-14 01:28:35 +0800

    fix: 添加缺失的激活状态查询异常处理

--- Hoi4BlueprintEditor/Views/LoadingView.xaml.cs
+++ Hoi4BlueprintEditor/Views/LoadingView.xaml.cs
@@ -1,19 +1,38 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using Hoi4BlueprintEditor.Services;
 using Microsoft.Extensions.DependencyInjection;
+using NLog;
+using MessageBox = iNKORE.UI.WPF.Modern.Controls.MessageBox;
 
 namespace Hoi4BlueprintEditor.Views;
 
 [RegisterTransient<LoadingView>]
 public sealed partial class LoadingView : UserControl
 {
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
     public LoadingView()
     {
         InitializeComponent();
-        App.Current.IsActivated.ContinueWith(async task =>
+        App.Current.IsActivated.ContinueWith(task =>
         {
-            Dispatcher.Invoke(() =>
+            Dispatcher.Invoke(async () =>
             {
+                if (task.Exception is not null)
+                {
+                    Log.Error(task.Exception, "查询设备激活状态失败");
+                    await MessageBox.ShowAsync(
+                        App.Current.MainWindow,
+                        "查询设备激活状态失败，请检查网络并稍后重试, 软件将自动关闭.",
+                        "错误",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error
+                    );
+                    App.Current.Shutdown();
+                    return;
+                }
+
                 var navigationService = App.Current.Services.GetRequiredService<NavigationService>();
                 if (task.IsCompletedSuccessfully && task.Result)
                 {
