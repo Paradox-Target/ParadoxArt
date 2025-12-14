abdfd250f289fee2cdfcf80bb829939bba30d7e6
textGamex <textGamex@outlook.com>
2025-12-15 01:19:07 +0800

    fix: 解决证书问题

--- Hoi4BlueprintEditor/Services/AuthService.cs
+++ Hoi4BlueprintEditor/Services/AuthService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Json;
+using System.Net.Security;
+using System.Security.Cryptography;
 using Hoi4BlueprintEditor.DTOs;
 
 namespace Hoi4BlueprintEditor.Services;
@@ -12,12 +14,28 @@ public sealed class AuthService : IDisposable
     public AuthService(DeviceService deviceService)
     {
         _deviceService = deviceService;
-        _client = new HttpClient();
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback = static (_, cert, _, errors) =>
+        {
+            if (errors == SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            string? actualThumbprint = cert?.GetCertHashString(HashAlgorithmName.SHA256);
+
+            return string.Equals(
+                actualThumbprint,
+                Private.ExpectedThumbprint,
+                StringComparison.OrdinalIgnoreCase
+            );
+        };
+        _client = new HttpClient(handler);
         _client.Timeout = TimeSpan.FromSeconds(15);
 #if DEBUG
         _client.BaseAddress = new Uri("https://localhost:7017/api/");
 #else
-        _client.BaseAddress = new Uri("https://localhost:7017/api/");
+        _client.BaseAddress = new Uri("https://47.96.168.220:7017/api/");
 #endif
     }
 
