1c08741b07a3db50c347354e8f7aad3b93127c0b
Supheria <supheria@gmail.com>
2025-12-16 23:20:54 +0800

    feat: 迁移 views initialization 未完成

---
+++ Hoi4BlueprintBuilder.Core/Views/Initialization/AppSettingsPageView.axaml.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Hoi4BlueprintBuilder.Core.Helpers;
+using Hoi4BlueprintBuilder.Core.Services;
+using Hoi4BlueprintBuilder.Core.ViewsModels.Initialization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hoi4BlueprintBuilder.Core.Views.Initialization;
+
+public partial class AppSettingsPageView : UserControl
+{
+    private readonly Frame _frame;
+
+    public AppSettingsPageView(Frame frame)
+    {
+        _frame = frame;
+        InitializeComponent();
+
+        DataContext = App.Current.Services.GetRequiredService<AppSettingsPageViewModel>();
+    }
+
+    private void Previous_OnClick(object sender, RoutedEventArgs e)
+    {
+        _frame.GoBack();
+    }
+
+    private void Next_OnClick(object sender, RoutedEventArgs e)
+    {
+        var settingsService = App.Current.Services.GetRequiredService<SettingsService>();
+        LanguageHelper.SetLanguage(settingsService.AppLanguage);
+        settingsService.SaveSettings();
+        App.Current.Services.GetRequiredService<NavigationService>().NavigateTo<MainControlView>();
+    }
+}
