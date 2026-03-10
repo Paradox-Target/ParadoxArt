b45fc7973677493974827bed850bcda144176925
textGamex <textGamex@outlook.com>
2026-03-10 19:23:31 +0800

    test: 调整测试

--- Hoi4BlueprintBuilder.UnitTests/Services/SpriteServiceTests.cs
+++ Hoi4BlueprintBuilder.UnitTests/Services/SpriteServiceTests.cs
@@ -72,27 +72,47 @@ public class SpriteServiceTests
     public void TryGetSpriteInfo_ShouldLoadSpritesFromTestData()
     {
         // Test 1: Basic sprite from test1.gfx
-        var result1 = _spriteService.TryGetSpriteInfo("GFX_test_1", out var info1);
-        Assert.That(result1, Is.True);
-        Assert.That(info1, Is.Not.Null);
-        Assert.That(info1.Name, Is.EqualTo("GFX_test_1"));
-        Assert.That(info1.RelativePath, Is.EqualTo("gfx/GFX_test_1.dds"));
-        Assert.That(info1.TotalFrames, Is.EqualTo(1));
+        bool result1 = _spriteService.TryGetSpriteInfo("GFX_test_1", out var info1);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result1, Is.True);
+            Assert.That(info1, Is.Not.Null);
+        }
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(info1.Name, Is.EqualTo("GFX_test_1"));
+            Assert.That(info1.RelativePath, Is.EqualTo("gfx/GFX_test_1.dds"));
+            Assert.That(info1.TotalFrames, Is.EqualTo(1));
+        }
 
-        var result2 = _spriteService.TryGetSpriteInfo("GFX_test_2", out var info2);
-        Assert.That(result2, Is.True);
-        Assert.That(info2, Is.Not.Null);
-        Assert.That(info2.Name, Is.EqualTo("GFX_test_2"));
-        Assert.That(info2.RelativePath, Is.EqualTo("gfx/GFX_test_2.dds"));
-        Assert.That(info2.TotalFrames, Is.EqualTo(1));
+        bool result2 = _spriteService.TryGetSpriteInfo("GFX_test_2", out var info2);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result2, Is.True);
+            Assert.That(info2, Is.Not.Null);
+        }
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(info2.Name, Is.EqualTo("GFX_test_2"));
+            Assert.That(info2.RelativePath, Is.EqualTo("gfx/GFX_test_2.dds"));
+            Assert.That(info2.TotalFrames, Is.EqualTo(1));
+        }
 
         // Test 2: Sprite from test2.gfx
-        var result3 = _spriteService.TryGetSpriteInfo("GFX_test_3", out var info3);
-        Assert.That(result3, Is.True);
-        Assert.That(info3, Is.Not.Null);
-        Assert.That(info3.Name, Is.EqualTo("GFX_test_3"));
-        Assert.That(info3.RelativePath, Is.EqualTo("gfx/GFX_test_3.dds"));
-        Assert.That(info3.TotalFrames, Is.EqualTo(1));
+        bool result3 = _spriteService.TryGetSpriteInfo("GFX_test_3", out var info3);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result3, Is.True);
+            Assert.That(info3, Is.Not.Null);
+        }
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(info3.Name, Is.EqualTo("GFX_test_3"));
+            Assert.That(info3.RelativePath, Is.EqualTo("gfx/GFX_test_3.dds"));
+            Assert.That(info3.TotalFrames, Is.EqualTo(1));
+        }
 
         // Test 3: Sprite with frames from test2.gfx
         var result4 = _spriteService.TryGetSpriteInfo("GFX_test_4", out var info4);
@@ -106,8 +126,11 @@ public class SpriteServiceTests
     [Test]
     public void TryGetSpriteInfo_ShouldReturnFalse_WhenKeyDoesNotExist()
     {
-        var result = _spriteService.TryGetSpriteInfo("NON_EXISTENT_SPRITE", out var info);
-        Assert.That(result, Is.False);
-        Assert.That(info, Is.Null);
+        bool result = _spriteService.TryGetSpriteInfo("NON_EXISTENT_SPRITE", out var info);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(result, Is.False);
+            Assert.That(info, Is.Null);
+        }
     }
 }
