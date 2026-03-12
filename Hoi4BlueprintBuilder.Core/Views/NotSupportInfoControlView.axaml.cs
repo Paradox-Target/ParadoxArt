cf9a3549a1a374513f32a9aae20c4143193c6909
textGamex <textGamex@outlook.com>
2026-03-12 18:09:56 +0800

    ui: 美化不支持文件格式页面

--- Hoi4BlueprintBuilder.Core/Views/NotSupportInfoControlView.axaml.cs
+++ Hoi4BlueprintBuilder.Core/Views/NotSupportInfoControlView.axaml.cs
@@ -6,6 +6,10 @@ namespace Hoi4BlueprintBuilder.Core.Views;
 [RegisterTransient<NotSupportInfoControlView>]
 public sealed partial class NotSupportInfoControlView : UserControl, ITabViewItem
 {
+    public string Header { get; }
+    public string FilePath { get; }
+    public string ToolTip { get; }
+
     public NotSupportInfoControlView(UserStatusService userStatusService)
     {
         InitializeComponent();
@@ -15,9 +19,6 @@ public sealed partial class NotSupportInfoControlView : UserControl, ITabViewIte
         Header = item.Name;
         FilePath = item.FullPath;
         ToolTip = item.FullPath;
+        FilePathBlock.Text = item.FullPath;
     }
-
-    public string Header { get; }
-    public string FilePath { get; }
-    public string ToolTip { get; }
 }
