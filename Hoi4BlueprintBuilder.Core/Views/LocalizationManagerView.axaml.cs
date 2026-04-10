a31caa173888ba0107b234b0a460990be45aac14
textGamex <textGamex@outlook.com>
2026-04-10 16:24:32 +0800

    feat: 本地化管理器Tooltip显示文件路径

--- Hoi4BlueprintBuilder.Core/Views/LocalizationManagerView.axaml.cs
+++ Hoi4BlueprintBuilder.Core/Views/LocalizationManagerView.axaml.cs
@@ -1,6 +1,11 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Templates;
 using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Layout;
 using FluentAvalonia.UI.Controls;
+using Hoi4BlueprintBuilder.Core.Models.Localization;
 using Hoi4BlueprintBuilder.Core.ViewsModels;
 
 namespace Hoi4BlueprintBuilder.Core.Views;
@@ -37,12 +42,49 @@ public sealed partial class LocalizationManagerView : UserControl, ITabViewItem
     {
         foreach (var lang in _viewModel.SupportedLanguages)
         {
-            var column = new DataGridTextColumn
+            var column = new DataGridTemplateColumn
             {
                 Header = lang.ToString(),
-                Binding = new Binding($"[{lang}]") { Mode = BindingMode.TwoWay },
                 Width = new DataGridLength(150, DataGridLengthUnitType.Pixel),
-                IsReadOnly = false
+                IsReadOnly = false,
+                // 单元格非编辑状态时的模板
+                CellTemplate = new FuncDataTemplate<object>(
+                    (_, _) =>
+                    {
+                        var textBlock = new TextBlock
+                        {
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(12, 0)
+                        };
+
+                        // 绑定显示的文本
+                        textBlock.Bind(TextBlock.TextProperty, new Binding($"[{lang}]"));
+                        // 绑定 ToolTip，显示 FilePath
+                        textBlock.Bind(
+                            Avalonia.Controls.ToolTip.TipProperty,
+                            new Binding(".")
+                            {
+                                Converter = new FuncValueConverter<LocalizationRow, string?>(row =>
+                                    row?.Languages.Find(l => l.Language == lang)?.FilePath
+                                )
+                            }
+                        );
+
+                        return textBlock;
+                    }
+                ),
+                // 单元格编辑状态时的模板
+                CellEditingTemplate = new FuncDataTemplate<object>(
+                    (_, _) =>
+                    {
+                        var textBox = new TextBox();
+                        textBox.Bind(
+                            TextBox.TextProperty,
+                            new Binding($"[{lang}]") { Mode = BindingMode.TwoWay }
+                        );
+                        return textBox;
+                    }
+                )
             };
             MainDataGrid.Columns.Add(column);
         }
