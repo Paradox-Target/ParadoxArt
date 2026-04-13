fe12cb7e80eee9d8b412bc8d78b9190ff1e67a81
textGamex <textGamex@outlook.com>
2026-04-13 23:36:07 +0800

    feat: CI/CD中添加 Linux 端

---
+++ Hoi4BlueprintBuilder.Linux/Program.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Hoi4BlueprintBuilder.Core;
+using Hoi4BlueprintBuilder.Core.Helpers;
+using NLog;
+using Velopack;
+
+namespace Hoi4BlueprintBuilder.Linux;
+
+public sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        try
+        {
+            NLogSetupHelper.Setup();
+            VelopackApp.Build().Run();
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            // 不写成静态字段是因为创建静态字段时未运行初始化日志组件的代码
+            var log = LogManager.GetCurrentClassLogger();
+            log.Error(e, "程序发生未捕获错误");
+        }
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseR3();
+    }
+}
